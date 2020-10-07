e3b93a5ad41842427172fd55fe96ca99ef60df69
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-10-07 20:28:36 +0530

    add ability to create stack from linkedList

---
+++ src/main/java/com/GenericsInLinkedList/MyStack.java
@@ -0,0 +1,23 @@
+package com.GenericsInLinkedList;
+
+public class MyStack {
+
+	private MyLinkedList myLinkedList;
+	
+	public MyStack() {
+		myLinkedList = new MyLinkedList();
+	}
+	
+	public void push(INode myNode) {
+		myLinkedList.addFromTop(myNode);
+	}
+
+	public INode peak() {
+		return myLinkedList.getHead();
+	}
+
+	public void printStack() {
+		myLinkedList.printMyNodes();	
+	}
+	
+}
