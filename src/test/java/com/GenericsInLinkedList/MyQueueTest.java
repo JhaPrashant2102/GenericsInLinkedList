c29fc48a1a243e2907fcfccd3522cd7510bdd7d7
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-10-07 21:38:22 +0530

    add dequeue, size, isEmpty to MyQueue class

--- src/test/java/com/GenericsInLinkedList/MyQueueTest.java
+++ src/test/java/com/GenericsInLinkedList/MyQueueTest.java
@@ -19,5 +19,47 @@ public class MyQueueTest {
 		myQueue.printQueue();
 		assertEquals(myFirstNode,myNode);
 	}
+	
+	@Test
+	public void given3NodesWhenPoppedFromQueueShouldMatchFirstAddedNode() {
+		MyQueue myQueue = new MyQueue();
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+		myQueue.enqueue(myFirstNode);
+		myQueue.enqueue(mySecondNode);
+		myQueue.enqueue(myThirdNode);
+		INode myNode = myQueue.dequeue();
+		myQueue.printQueue();
+		assertEquals(myThirdNode,myNode);
+	}
+	
+	@Test
+	public void given3Nodes_shouldReturnSize3() {
+		MyQueue myQueue = new MyQueue();
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+		myQueue.enqueue(myFirstNode);
+		myQueue.enqueue(mySecondNode);
+		myQueue.enqueue(myThirdNode);
+		int size = myQueue.size();
+		myQueue.printQueue();
+		assertEquals(3,size);
+	}
+	
+	@Test
+	public void given3Nodes_shouldIsEmptyFalse() {
+		MyQueue myQueue = new MyQueue();
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+		myQueue.enqueue(myFirstNode);
+		myQueue.enqueue(mySecondNode);
+		myQueue.enqueue(myThirdNode);
+		boolean check = myQueue.isEmpty();
+		myQueue.printQueue();
+		assertFalse(check);
+	}
 
 }
