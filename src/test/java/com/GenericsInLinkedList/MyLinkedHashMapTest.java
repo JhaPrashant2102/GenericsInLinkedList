b3575d4a49323625204f3e53ac7628214fed1a98
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-10-10 21:30:32 +0530

    added feature to remove word from hashmap

--- src/test/java/com/GenericsInLinkedList/MyLinkedHashMapTest.java
+++ src/test/java/com/GenericsInLinkedList/MyLinkedHashMapTest.java
@@ -19,10 +19,25 @@ public class MyLinkedHashMapTest {
 		}
 		System.out.println(myLinkedListHashMap);
 		Integer frequency = myLinkedListHashMap.get("paranoid");
-		if(frequency==null)
-			System.out.println("f is null");
-		assertEquals(3, (int)frequency);
-		
+		assertEquals(3, (int)frequency);	
 	}
 
+	@Test
+	public void givenAFilledMapWhenAvoidableWordIsRemovedShouldReturnFrequencyZero() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but"+
+					" because they keep putting themselves deliberately into paranoid avoidable situations";
+		MyLinkedListHashMap<String,Integer> myLinkedListHashMap = new MyLinkedListHashMap<>();
+		String words[] = sentence.toLowerCase().split(" ");
+		for(String word : words) {
+			Integer value =  myLinkedListHashMap.get(word);
+			value = (value==null)?1:++value;
+			myLinkedListHashMap.add(word,value);
+		}
+		System.out.println(myLinkedListHashMap);
+		myLinkedListHashMap.remove("avoidable");
+		Integer frequency = myLinkedListHashMap.get("avoidable");
+		if(frequency==null)
+			frequency = 0;
+		assertEquals(0,(int)frequency);	
+	}
 }
